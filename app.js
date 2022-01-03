cc314efa728d4c1f6096ac5fcbd4bef4c17d4110
Htet-Wai-Yan <h.waiyan101@gmail.com>
2022-01-03 19:53:54 +0630

    Local storage added but not satisfy

--- app.js
+++ app.js
@@ -4,47 +4,58 @@ let theList = document.querySelector("#theList");
 let theField = document.querySelector("#newItem");
 let theAlert = document.querySelector("#theAlert");
 
+let notesInLocalStorage = localStorage.getItem('note')
+
+let noteObj = notesInLocalStorage !== null ? JSON.parse(notesInLocalStorage): []
+
 // Submit event
 theForm.addEventListener("submit", (event) => {
-  //   disable form default refresh behaviour
+  // disable form default refresh behaviour
   event.preventDefault();
-  addItem(theField.value);
+
+  noteObj.push(theField.value.trim())
+  localStorage.setItem('note', JSON.stringify(noteObj))
+
+  addItem(noteObj)
+
+  theField.value = ""
+
 });
 
-function addItem(inputValue) {
-  //   the HTML template
-  theNewItem = `
-    <li class = "my-2">
-      <div>
-        <input class="form-check-input me-1" type="checkbox" onchange="toggleCheckbox(this)" /> ${inputValue}
-      </div>
-      <i class = "fas fa-trash-alt" id = "deleteBtn" onclick = "deleteNote(this)" style = "cursor: pointer"></i>
-    </li>
-  `;
-
-  if (theField.value !== "") {
-    //   add "where" and "item"
-    theList.insertAdjacentHTML("beforeend", theNewItem);
-
-    //   empty field after submit
-    theField.value = "";
-
-    //   focus field again after submit
-    theField.focus();
-
-    //   toggle alert
-    theAlert.classList.add("d-none");
-  } else {
-    theAlert.classList.remove("d-none");
-  }
-}
-
-// toggle css class when the checkbox is toggle
-function toggleCheckbox(click) {
-  click.parentElement.classList.toggle("checkDone");
-}
-
-// delete items when trash icon is click
-function deleteNote(click) {
+function deleteItem(click) {
   click.parentElement.remove()
 }
+
+function addItem(noteObj) {
+  let li = `
+      <li class = "my-2">
+        <div>
+          <input class="form-check-input me-1" type="checkbox" /> ${noteObj.length == 0 ? noteObj[noteObj.length + 1] : noteObj[noteObj.length - 1]}
+        </div>
+        <i class = "fas fa-trash-alt" id = "deleteBtn" style = "cursor: pointer" onclick = "deleteItem(this)"></i>
+      </li>
+    `
+
+    theList.insertAdjacentHTML('beforeend', li)
+}
+
+function localStorageItem() {
+  noteObj.forEach(note => {
+    let li = `
+      <li class = "my-2">
+        <div>
+          <input class="form-check-input me-1" type="checkbox" /> ${note}
+        </div>
+        <i class = "fas fa-trash-alt" id = "deleteBtn" style = "cursor: pointer" onclick = "deleteItem(this)"></i>
+      </li>
+    `
+
+    theList.insertAdjacentHTML('beforeend', li)
+  })
+}
+
+localStorageItem()
+
+
+
+
